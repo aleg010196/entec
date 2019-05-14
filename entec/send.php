239bd958220421a1ccaeaee2d941e9a1387cd831
aleg010196 <48119524+aleg010196@users.noreply.github.com>
2019-05-14 09:53:26 +0500

    Update send.php

--- entec/send.php
+++ entec/send.php
@@ -10,7 +10,7 @@ $phone = trim($phone);
 echo $fio;
 echo "<br>";
 echo $email;
-if (mail("cirka010196@gmail.com", "Заявка с сайта", "ФИО:".$fullname.". E-mail: ".$phone ,"From: https://aleg010196.github.io/entec/entec/ \r\n"))
+if (mail("cirka010196@gmail.com", "Заявка с сайта", "ФИО:".$fullname.". E-mail: ".$phone ,"From: aleg010196@gmail.com \r\n"))
  {     echo "сообщение успешно отправлено"; 
 } else { 
     echo "при отправке сообщения возникли ошибки";
